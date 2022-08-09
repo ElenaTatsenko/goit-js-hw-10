9ed14ff02a0404357c308bfa138b13aed41bdf52
Elena Tatsenko <lena.harchenko24@gmail.com>
2022-08-09 16:20:24 +0300

    add css & functions

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 
 export default function fetchCountries(name) {
-  const variables = 'name,capital,population,flags,languages';
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=${variables}`).then(response => response.json());
+  const params = 'name,capital,population,flags,languages';
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=${params}`).then(response => response.json());
 }
--- src/renderCard.js
+++ src/renderCard.js
@@ -1,4 +1,3 @@
-
 export default function renderCard(params, card) {
   const markup = params
     .map(
@@ -9,20 +8,20 @@ export default function renderCard(params, card) {
         flags: { svg },
         languages,
       }) => 
-        `<div class="country-info__box"><img src="${svg}" alt="flag" width="30">
+        `<div class="country-info__card"><img src="${svg}" alt="flag" width="30">
     <h1 class="country-info__main-title">${official}</h1></div>
     <ul class="country-info__list">
     <li class="country-info__item">
      <h2 class="country-info___title">Capital:</h2>
-    <p class="country-info___text">${capital}</p>
+    <p class="country-info___description">${capital}</p>
     </li>
     <li class="country-info__item">
         <h2 class="country-info___title">Population:</h2>
-     <p class="country-info___text">${population}</p>
+     <p class="country-info___description">${population}</p>
     </li>
     <li class="country-info__item">
     <h2 class="country-info___title">Languages:</h2>
-     <p class="country-info___text">${Object.values(languages)}</p></li>
+     <p class="country-info___description">${Object.values(languages)}</p></li>
     </ul>`
     )
     .join('');
@@ -30,3 +29,28 @@ export default function renderCard(params, card) {
   return (card.innerHTML = markup);
 }
 
+//function createCard(item) {
+ //    return `<div class="country-info__card"><img src="${item.svg}" alt="flag" width="30">
+ //   <h1 class="country-info__main-title">${item.official}</h1></div>
+//    <ul class="country-info__list">
+ //   <li class="country-info__item">
+ //    <h2 class="country-info___title">Capital:</h2>
+ //   <p class="country-info___description">${item.capital}</p>
+ //   </li>
+ //   <li class="country-info__item">
+ //       <h2 class="country-info___title">Population:</h2>
+ //    <p class="country-info___description">${item.population}</p>
+//    </li>
+//    <li class="country-info__item">
+//    <h2 class="country-info___title">Languages:</h2>
+//     <p class="country-info___description">${Object.values(item.languages)}</p></li>
+ //   </ul>`
+//}
+//function genCont(array) {
+//  array.reduce((acc, item) => acc + createCard(item), "")
+//}
+
+//export default function renderCard(array) {
+//  const markup = genCont(array);
+ // card.insertAdjacentHTML("beforeend", markup)
+//}
